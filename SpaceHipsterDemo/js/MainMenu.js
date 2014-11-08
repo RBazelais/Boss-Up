93d05f8965d64ae5dedb1ea099f0c860b49aece9
edbles <edbles@gmail.com>
2014-11-08 13:27:09 -0500

    Added SpaceHipster Demo to directory

--- SpaceHipsterDemo/js/MainMenu.js
+++ SpaceHipsterDemo/js/MainMenu.js
@@ -1,9 +1,17 @@
-GhostBike.MainMenu = function(){};
+var SpaceHipster = SpaceHipster || {};
 
-GhostBike.MainMenu.prototype = {
+//title screen
+SpaceHipster.MainMenu = function(){};
+
+SpaceHipster.MainMenu.prototype = {
+  init: function(score) {
+    var score = score || 0;
+    this.highestScore = this.highestScore || 0;
+
+    this.highestScore = Math.max(score, this.highestScore);
+   },
   create: function() {
   	//show the space tile, repeated
-    /*
     this.background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'space');
     
     //give it speed in x
@@ -21,7 +29,6 @@ GhostBike.MainMenu.prototype = {
   
     var h = this.game.add.text(this.game.width/2, this.game.height/2 + 50, text, style);
     h.anchor.set(0.5);
-    */
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
